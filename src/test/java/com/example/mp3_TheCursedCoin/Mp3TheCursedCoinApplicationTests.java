6f64c12fb217815a93393291cf23703510e562b3
diaaaaaa <ite.diaa@gmail.com>
2018-03-23 12:15:35 +0100

    first adding

---
+++ src/test/java/com/example/mp3_TheCursedCoin/Mp3TheCursedCoinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mp3_TheCursedCoin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp3TheCursedCoinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
